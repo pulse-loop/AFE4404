382680e6013cdfbfff8fa661490e6ad8e6980f92
Fabio Cragnolini <fbcragnolini@gmail.com>
2022-07-27 11:55:00 +0200

    Added Clippy and #no_std directives.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+#![warn(clippy::nursery)]
+#![warn(clippy::cargo)]
+#![allow(clippy::multiple_crate_versions)]
+// Documentation lints
+#![warn(missing_docs)]
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(invalid_doc_attributes)]
+#![warn(rustdoc::all)]
+
+#![no_std]
